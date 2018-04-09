141d1521f86617bb55528a5aba25a7f296a41e47
Christophe Frattino <christophe@frattino.ch>
2018-04-09 16:24:36 +0200

    WIP attempt to establish a SSE connection and exchange messages with the server

--- src/app/sse-poc/sse-poc.component.ts
+++ src/app/sse-poc/sse-poc.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { EventSourcePolyfill } from 'ng-event-source';
+
 @Component({
   selector: 'app-sse-poc',
   templateUrl: './sse-poc.component.html',
@@ -13,7 +15,18 @@ export class SsePocComponent implements OnInit {
   }
 
   establishSSEConnection(): void {
-    console.info(`todo`)
+    let eventSource = new EventSourcePolyfill('http://localhost:8000/events/channel-1', {
+
+    });
+    eventSource.onmessage = (data => {
+        console.info(data);
+    });
+    eventSource.onopen = (a) => {
+        console.info("SSE connection established!");
+    };
+    eventSource.onerror = (e) => {
+      console.error(e);
+    }
   }
 
 }
