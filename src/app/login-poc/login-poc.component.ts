49bb5622ed7d3fbd371679a6d9b3e928e9fb37c9
Christophe Frattino <christophe.frattino@elca.ch>
2018-04-24 14:31:19 +0200

    Fully working front-end Implicit login

--- src/app/login-poc/login-poc.component.ts
+++ src/app/login-poc/login-poc.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+import { OAuthService } from 'angular-oauth2-oidc';
+
 @Component({
   selector: 'app-login-poc',
   templateUrl: './login-poc.component.html',
   styleUrls: ['./login-poc.component.css']
 })
-export class LoginPocComponent implements OnInit {
+export class LoginPocComponent {
 
-  constructor() { }
+  constructor(private oauthService: OAuthService) {
+  }
 
-  ngOnInit() {
+  public login() {
+      this.oauthService.initImplicitFlow();
+  }
+
+  public logoff() {
+      this.oauthService.logOut();
+  }
+
+  public get name() {
+      let claims = this.oauthService.getIdentityClaims();
+      if (!claims) return null;
+      return claims["preferred_username"];
   }
 
 }
