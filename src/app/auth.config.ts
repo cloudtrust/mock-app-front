d0a47ab20138dc30121221b6e0eb894660e3009f
Christophe Frattino <christophe.frattino@elca.ch>
2018-04-24 13:11:09 +0200

    OIDC config

---
+++ src/app/auth.config.ts
@@ -0,0 +1,17 @@
+import { AuthConfig } from 'angular-oauth2-oidc';
+
+export const authConfig: AuthConfig = {
+
+  // Url of the Identity Provider
+  issuer: 'https://ct-int-idp.dev.elca.ch/auth/realms/mock_realm',
+
+  // URL of the SPA to redirect the user to after login
+  redirectUri: window.location.origin + '/index.html',
+
+  // The SPA's id. The SPA is registerd with this id at the auth-server
+  clientId: 'mock-app',
+
+  // set the scope for the permissions the client should request
+  // The first three are defined by OIDC. The 4th is a usecase-specific one
+  scope: 'openid profile email voucher',
+}
