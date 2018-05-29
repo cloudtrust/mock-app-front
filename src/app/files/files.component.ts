4eff16635a1a2c08b1b0e5feeb7357079be97c3d
Christophe Frattino <christophe.frattino@elca.ch>
2018-05-29 14:46:38 +0200

    Show medical files in a table on the Medical Files page

--- src/app/files/files.component.ts
+++ src/app/files/files.component.ts
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FilesComponent implements OnInit {
 
-  constructor() { }
+  dtOptions: DataTables.Settings = {};
+
+  constructor() {
+  }
 
   ngOnInit() {
+    this.dtOptions = {
+      ajax: {
+        "url": "http://localhost:8000/files",
+        "dataSrc": ""
+      },
+      lengthChange: false,
+      searching: false,
+      columns: [{
+        title: 'ID',
+        data: 'id'
+      }, {
+        title: 'Patient AVS Number',
+        data: 'patientAvsNumber'
+      }, {
+        title: 'Doctor ID',
+        data: 'doctorId'
+      }, {
+        title: 'Data',
+        data: 'data'
+      }]
+    };
   }
 
 }
